ea7f409cb7d9cd14157b5dacabeb333b9f565694
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2019-06-04 16:58:16 +0200

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="datetime_functions",
-    version="0.0.2",
+    version="0.0.3",
     author="Bee Group",
     description="Simple functions to treat datetimes",
     long_description=long_description,
